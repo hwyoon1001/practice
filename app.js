143d4f0f70f0af774294186f1fbf4a822f0988ea
hwyoon1001 <94165184+hwyoon1001@users.noreply.github.com>
2022-02-10 16:44:15 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,86 @@
+var albumBucketName = 'nmlt201021';
+var bucketRegion = 'ap-northeast-2';
+var IdentityPoolId = 'ap-northeast-2:367dd400-0773-419e-b5db-8b97004e5c64';
+
+AWS.config.update({
+  region: bucketRegion,
+  credentials: new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: IdentityPoolId
+  })
+});
+
+var s3 = new AWS.S3({
+  apiVersion: '2006-03-01',
+  params: {
+    Bucket: albumBucketName
+  }
+});
+
+function upload_to_db(img_location) {
+    var article_id = document.querySelector("#Name").value;
+    var date = document.querySelector("#date").value;
+ 
+    var Item = {
+        'article_id': article_id,
+        'date': date,
+        'img_source': img_location,
+    }
+    console.log(Item);
+ 
+    const URL = "https://l9biqyi3wk.execute-api.ap-northeast-2.amazonaws.com/stage/board";
+ 
+    fetch(URL, {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Access-Control-Allow-Origin' : '*',
+        },
+        body: JSON.stringify({
+            "TableName": "board-test",
+            Item
+        })
+    }).then(resp => console.log(resp))
+        .catch(err => console.log(err))
+}
+ 
+function add_article_with_photo(albumName) {
+    var files = document.getElementById("article_image").files;
+    if (!files.length) {
+        return alert("Please choose a file to upload first.");
+    }
+    var file = files[0];
+    var fileName = file.name;
+    var albumPhotosKey = encodeURIComponent(albumName) + "/";
+    var albumPhotosKey = albumName + "/";
+ 
+    var photoKey = albumPhotosKey + fileName;
+ 
+    // Use S3 ManagedUpload class as it supports multipart uploads
+    var upload = new AWS.S3.ManagedUpload({
+        params: {
+        Bucket: albumBucketName,
+        Key: photoKey,
+        Body: file
+        }
+    });
+ 
+    var promise = upload.promise();
+ 
+    let img_location;
+ 
+    promise.then(
+        function(data) {
+        //이미지 파일을 올리고 URL을 받아옴
+        img_location = JSON.stringify(data.Location).replaceAll("\"","");
+        // console.log(img_location);
+        
+        upload_to_db(img_location);
+ 
+        return alert("Successfully uploaded photo.");;
+        },
+        function(err) {
+            console.log(err);
+        return alert("There was an error uploading your photo: ", err.message);
+        }
+    );
+    }
