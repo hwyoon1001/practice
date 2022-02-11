b34cb9dbcb0958f12569c8be7e0ad51c4e22b56a
hwyoon1001 <94165184+hwyoon1001@users.noreply.github.com>
2022-02-11 19:09:12 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,142 +1,60 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Article_Add</title>
-        <style>
-        </style>
-            <!--켜지면 get() 함수를 실행함.  -->
-            <body onLoad="get()">
-                <div id="articles">
-            
-                </div>
+var albumBucketName = 'nmlt201021';
+var bucketRegion = 'ap-northeast-2';
+var IdentityPoolId = 'ap-northeast-2:367dd400-0773-419e-b5db-8b97004e5c64';
+
+AWS.config.update({
+  region: bucketRegion,
+  credentials: new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: IdentityPoolId
+  })
+});
+
+var s3 = new AWS.S3({
+  apiVersion: '2006-03-01',
+  params: {
+    Bucket: albumBucketName
+  }
+});
 
  
-    </head>
- 
-    <body>
-        <div class="Top">
-  
-        </div>
-        <div style="text-align: center;"><h1>글 등록 페이지</h1></div>
- 
-        <form name="article_add_form" method="POST">
-            <table width="940" style="padding:5px 0 5px 0; ">
-               <tr height="2"><td colspan="2"></td></tr>
-               <tr>
-                  <th>name</th>
-                  <td><input type="text" id="name" required></td>
-               </tr>
-               <tr>
-                  <th>Title</th>
-                  <td><input type="text" id="title" required></td>
-                </tr>
-                    <th>사진</th>
-                    <td><input type="file" id="article_image" name="filename"></td>
-                  </tr>
-                    <tr>
-                      <td colspan="3" style="text-align: center;">
-                        <input type="button" value="등록" onClick="upload_to_db()" />
-                        <input type="reset" value="취소">
-                     </td>
-                    </table>
-                   </form>
- 
-        </div>
-         
-        <script src="https://sdk.amazonaws.com/js/aws-sdk-2.283.1.min.js"></script>
-        <script src="./app.js"></script>
-        
-        <script>
-            /* HTML노드 추가 함수 */
-            function createNode(element){
-                return document.createElement(element);
-            }
-            /* HTML노드에 자녀 추가 함수 */
-            function append(parent, el){
-                return parent.appendChild(el);
-            }
-            /* CSS 속성 추가 함수 */
-            function addStyle(styles) { 
-              
-              /* Create style element */ 
-              var css = document.createElement('style'); 
-              css.type = 'text/css'; 
-         
-              if (css.styleSheet)  
-                  css.styleSheet.cssText += styles; 
-              else  
-                  css.appendChild(document.createTextNode(styles)); 
-                
-              /* Append style to the head element */ 
-              document.getElementsByTagName("head")[0].appendChild(css); 
-          }
-          var elem = document.getElementById('name');
-            /* API 게이트웨이로 매물리스트 정보 가져오는 함수 */ 
-            const URL = "https://l9biqyi3wk.execute-api.ap-northeast-2.amazonaws.com/stage/board" ;
-
-            
-            
-          function get() {
-            fetch(URL, {
-                headers: {
-                  'Accept': 'application/json'
-                }
-              }).then(res => res.json())
-              .then(data => console.log(data))
-              .catch(err => console.error(err));
-          }
-              
-        
-        var Item = {
-              'name': "",
-              'title': "",
-              'img_source': "",
-          }
-          
-      function upload_to_db(img_location) {
-        var name = document.querySelector("#name").value;
-        var title = document.querySelector("#title").value;
-       Item = {
-        'name': name,
-        'title': title,
-        'img_source': img_location,
-   
-       }
-
-    post()
+function add_article_with_photo(albumName) {
+    var files = document.getElementById("article_image").files;
+    if (!files.length) {
+        return alert("Please choose a file to upload first.");
     }
-
-      function post() {
-      fetch(URL, {
-          method: "POST",
-          headers: {
-            'Accept': 'application/json',
-          },
-          body: JSON.stringify({
-            "TableName": "board-test",
-            Item
-          })
-        }).then(resp => console.log(resp))
-        .catch(err => console.log(err))
+    var file = files[0];
+    var fileName = file.name;
+    var albumPhotosKey = encodeURIComponent(albumName) + "/";
+    var albumPhotosKey = albumName + "/";
+ 
+    var photoKey = albumPhotosKey + fileName;
+ 
+    // Use S3 ManagedUpload class as it supports multipart uploads
+    var upload = new AWS.S3.ManagedUpload({
+        params: {
+        Bucket: albumBucketName,
+        Key: photoKey,
+        Body: file
+        }
+    });
+ 
+    var promise = upload.promise();
+ 
+    let img_location;
+ 
+    promise.then(
+        function(data) {
+        //이미지 파일을 올리고 URL을 받아옴
+        img_location = JSON.stringify(data.Location).replaceAll("\"","");
+        // console.log(img_location);
+        
+        upload_to_db(img_location);
+ 
+        return alert("Successfully uploaded photo.");;
+        },
+        function(err) {
+            console.log(err);
+        return alert("There was an error uploading your photo: ", err.message);
+        }
+    );
     }
-              
-              
-
-            
-      function submitToAPI(e){
-          e.preventDefault();
-          add_article_with_photo('images');
-       }
-
-        </script>
- 
-    </body>
- 
-    <head>
- 
-    </head>
- 
-</html>
-
- 
