a90e2d5bf17d03febcf30c582ceeb0190f6048a0
hwyoon1001 <94165184+hwyoon1001@users.noreply.github.com>
2022-02-14 15:17:59 +0900

    Update app.js

--- app.js
+++ app.js
@@ -16,18 +16,120 @@ var s3 = new AWS.S3({
   }
 });
 
+function listAlbums() {
+  s3.listObjects({
+    Delimiter: '/'
+  }, function (err, data) {
+    if (err) {
+      return alert('There was an error listing your directory: ' + err.message);
+    } else {
+      console.log('앨범', data.CommonPrefixes)
+      var albums = data.CommonPrefixes.map(function (commonPrefix) {
+        var prefix = commonPrefix.Prefix;
+        var albumName = decodeURIComponent(prefix.replace('/', ''));
+        return getHtml([
+          '<li>',
+          '<span onclick="deleteCheck(\'' + albumName + '\')">X</span>',
+          '<span onclick="viewAlbum(\'' + albumName + '\')">',
+          albumName,
+          '</span>',
+          '</li>'
+        ]);
+      });
+      var message = albums.length ?
+        getHtml([
+          '<p>Click on directory name to view it.</p>',
+          '<p>Click on the X to delete the directory.</p>'
+        ]) :
+        '<p>You do not have any albums. Please Create Directory.';
+      var htmlTemplate = [
+        '<h2>Directory</h2>',
+        message,
+        '<ul>',
+        getHtml(albums),
+        '</ul>',
+        '<button onclick="createAlbum(prompt(\'Enter directory Name:\'))">',
+        'Create New Directory',
+        '</button>'
+      ]
+      document.getElementById('app').innerHTML = getHtml(htmlTemplate);
+    }
+  });
+}
+
+function viewAlbum(albumName) {
+  var albumPhotosKey = encodeURIComponent(albumName) + '/';
+  s3.listObjects({
+    Prefix: albumPhotosKey
+  }, function (err, data) {
+    if (err) {
+      return alert('There was an error viewing your directory: ' + err.message);
+    }
+    // 'this' references the AWS.Response instance that represents the response
+    var href = this.request.httpRequest.endpoint.href;
+    var bucketUrl = href + albumBucketName + '/';
+    console.log('앨범', data.Contents)
+
+    var photos = data.Contents.map(function (photo) {
+      var photoKey = photo.Key;
+      var photoUrl = bucketUrl + encodeURIComponent(photoKey);
+      return getHtml([
+        '<span>',
+        '<div>',
+        '</div>',
+        '<div>',
+        '<span onclick="deleteCheckFile(\'' + albumName + "','" + photoKey + '\')">',
+        '[X]',
+        '</span>',  
+        '<span>',
+        photoKey.replace(albumPhotosKey, ''),
+        '<span onclick="preprocessing()">',
+        '[preprocessing]',
+        '</span>',
+        '</span>',
+
+        '</div>',
+        '</span>',
+      ]);
+    });
+    var message = photos.length -1?
+      '<p>Click on the X to delete the file</p>' :
+      '<p>You do not have any file in this directory. Please add file.</p>';
+    var htmlTemplate = [
+      '<h2>',
+      'Directory: ' + albumName,
+      '</h2>',
+      message,
+      '<div>',
+      getHtml(photos),
+      '</div>',
+      '<input id="photoupload" type="file" accept="/*">',
+      '<button id="addphoto" onclick="addPhoto(\'' + albumName + '\')">',
+      'Upload',
+      '</button>',
+      '<button onclick="listAlbums()">',
+      'Back',
+      '</button>',
+
+    ]
+    document.getElementById('app').innerHTML = getHtml(htmlTemplate);
+  });
+}
+
  
 function add_article_with_photo(albumName) {
     var files = document.getElementById("article_image").files;
     if (!files.length) {
         return alert("Please choose a file to upload first.");
     }
-    var file = files[0];
-    var fileName = file.name;
-    var albumPhotosKey = encodeURIComponent(albumName) + "/";
-    var albumPhotosKey = albumName + "/";
- 
-    var photoKey = albumPhotosKey + fileName;
+    
+     for (var i = 0; i < uploadFiles.files.length; i++) {
+        var file = uploadFiles.files[i];
+        var fileName = file.name;
+        var albumPhotosKey = encodeURIComponent(albumName) + "/";
+        var albumPhotosKey = albumName + "/";
+     
+        var photoKey = albumPhotosKey + fileName;
  
     // Use S3 ManagedUpload class as it supports multipart uploads
     var upload = new AWS.S3.ManagedUpload({
@@ -57,4 +159,5 @@ function add_article_with_photo(albumName) {
         return alert("There was an error uploading your photo: ", err.message);
         }
     );
+    }
     }
